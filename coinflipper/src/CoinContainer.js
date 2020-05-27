fa2b2157881b919048541e4e370d6d196a105272
huangfuin11 <huangfuin1101@yahoo.com.tw>
2020-05-28 02:44:01 +0800

    added helpers and Coin component

--- coinflipper/src/CoinContainer.js
+++ coinflipper/src/CoinContainer.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import Coin from "./Coin";
+import { choice } from "./helpers";
 
 class CoinContainer extends Component {
   static defaultProps = {
@@ -15,11 +17,29 @@ class CoinContainer extends Component {
       nHeads: 0,
       nTails: 0
     };
+    this.handleClick = this.handleClick.bind(this);
+  }
+  flipCoin() {
+    const newCoin = choice(this.props.coins);
+    this.setState(st => {
+      return {
+        currCoin: newCoin,
+        nFlips: st.nFlips + 1,
+        nHeads: st.nHeads + (newCoin.side === "heads" ? 1 : 0),
+        nTails: st.nTails + (newCoin.side === "tails" ? 1 : 0)
+      };
+    });
+
+  }
+  handleClick(e) {
+    this.flipCoin();
   }
   render() {
     return (
       <div className='CoinContainer'>
         <h2>Let's Flip A Coin!</h2>
+        {this.state.currCoin && <Coin info={this.state.currCoin} />}
+        <button onClick={this.handleClick}>Flip Me!</button>
         <p>
           Out of {this.state.nFlips} flips, there have been {this.state.nHeads}{" "}
           heads and {this.state.nTails} tails.
